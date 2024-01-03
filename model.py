08a286819a2ce34a5ba69a39930fac14a9427857
hsmint <hsmint.hong@gmail.com>
2024-01-03 10:56:13 +0900

    Model of MNIST 2NN

---
+++ model.py
@@ -0,0 +1,24 @@
+from torch import nn
+import torch.nn.functional as F
+import torch
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, 5, 1)
+        self.fc1 = nn.Linear(9216, 512)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        output = F.log_softmax(x, dim=1)
+        return output
