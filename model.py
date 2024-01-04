4f55a484f7a808de8809402f9eea629994f25698
hsmint <hsmint.hong@gmail.com>
2024-01-04 11:08:55 +0900

    Fix the model softmax

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=1)
         self.conv2 = nn.Conv2d(32, 64, 5, 1)
         self.fc1 = nn.Linear(9216, 512)
+        self.fc2 = nn.Linear(512, 10)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -20,5 +21,7 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
